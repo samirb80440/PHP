c43287db14cdf8625580fc7af72eb099279194f3
Samir <samirb80440@gmail.com>
2024-05-14 16:42:45 +0200

    nvmodif

--- index.php
+++ index.php
@@ -6,20 +6,50 @@
     <title>PHP test</title>
 </head>
 <body>
-  <thead>
-    <tr>
-   <th><?php 
- for ($i = 0; $i <= 9; $i++)
- {
-     Echo $i.'<br>';
-     
-     for ($j = 0; $j <= 9; $j++)
-     {
-         echo $i.' x '.$j.' = '.$i*$j.'<br>';
-     }
- }
-  ?></th 
-  </tr>
-  </thead>
+<?php
+// $NbrCol : le nombre de colonnes
+// $NbrLigne : le nombre de lignes
+$NbrCol = 9;
+$NbrLigne = 9;
+// --------------------------------------------------------
+// on affiche en plus sur les 1ere ligne et 1ere colonne 
+// les valeurs a multiplier (dans des cases en couleur)
+// le tableau fera donc 10 x 10
+// --------------------------------------------------------
+echo '<table border="1" width="400">';
+// 1ere ligne (ligne 0)
+   echo '<tr>';
+   echo '<td>0</td>';
+   for ($j=1; $j<=$NbrCol; $j++) {
+      echo '<td">'.$j.'</td>';
+   }
+   echo '</tr>';
+// -------------------------------------------------------
+// lignes suivantes
+for ($i=1; $i<=$NbrLigne; $i++) {
+   echo '<tr>';
+   for ($j=1; $j<=$NbrCol; $j++) {
+       // 1ere colonne (colonne 0)
+      if ($j==1) {
+         echo '<td>'.$i.'</td>';
+      }
+       // colonnes suivantes
+         if ($i==$j) {
+            echo '<td>';
+         } else {
+            echo '<td>';
+         }
+       // ------------------------------------------
+       // AFFICHAGE ligne $i, colonne $j
+      echo $i*$j;
+       // ------------------------------------------
+      echo '</td>';
+   }
+   echo '</tr>';
+   $j=1;
+}
+echo '</table>';
+?>
+</body></html> 
 </body>
 </html>
