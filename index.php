30432d1555614fb6d29d89d9a7f742167a977c8d
Samir <samirb80440@gmail.com>
2024-05-17 08:24:55 +0200

    nvmodif

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ echo '</table>';
 */
 
 
-$capitales = array(
+/*$capitales = array(
    "Bucarest" => "Roumanie",
    "Bruxelles" => "Belgique",
    "Oslo" => "Norvège",
@@ -160,11 +160,25 @@ foreach($capitales_trie as $key=>$value){
 */
 
 
+$departements = array(
+   "Hauts-de-france" => array("Aisne", "Nord", "Oise", "Pas-de-Calais", "Somme"),
+   "Bretagne" => array("Côtes d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan"),
+   "Grand-Est" => array("Ardennes", "Aube", "Marne", "Haute-Marne", "Meurthe-et-Moselle", "Meuse", "Moselle", "Bas-Rhin", "Haut-Rhin", "Vosges"),
+   "Normandie" => array("Calvados", "Eure", "Manche", "Orne", "Seine-Maritime")
+);
 
 
+asort($departements);
 
+foreach( $departements as $key => $value )
+{
+echo $key .count($departements[$key]). ': <br>';
 
+foreach( $value as $valeur )
+echo ' ' . $valeur . '<br>';
 
+echo '<br>';
+}
 
 
 
