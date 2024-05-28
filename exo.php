c0800a7bab8425aed9c766a2ab64964f100e2b03
Samir <samirb80440@gmail.com>
2024-05-28 15:37:21 +0200

    nvmodif

---
+++ exo.php
@@ -0,0 +1,33 @@
+<?php
+$NbrCol = 9;
+$NbrLigne = 9;
+echo '<table border="1" width="400">';
+
+   echo '<tr>';
+   echo '<td>0</td>';
+   for ($j=1; $j<=$NbrCol; $j++) {
+      echo '<td">'.$j.'</td>';
+   }
+   echo '</tr>';
+
+
+for ($i=1; $i<=$NbrLigne; $i++) {
+   echo '<tr>';
+   for ($j=1; $j<=$NbrCol; $j++) {
+      
+      if ($j==1) {
+         echo '<td>'.$i.'</td>';
+      }
+         if ($i==$j) {
+            echo '<td>';
+         } else {
+            echo '<td>';
+         }
+      echo $i*$j;
+      echo '</td>';
+   }
+   echo '</tr>';
+   $j=1;
+}
+echo '</table>';
+?>
--- index.php
+++ index.php
@@ -14,38 +14,8 @@ while ($a < 150) {
     $a=$a+2; 
 } */
 
-/*$NbrCol = 9;
-$NbrLigne = 9;
-echo '<table border="1" width="400">';
-
-   echo '<tr>';
-   echo '<td>0</td>';
-   for ($j=1; $j<=$NbrCol; $j++) {
-      echo '<td">'.$j.'</td>';
-   }
-   echo '</tr>';
 
 
-for ($i=1; $i<=$NbrLigne; $i++) {
-   echo '<tr>';
-   for ($j=1; $j<=$NbrCol; $j++) {
-      
-      if ($j==1) {
-         echo '<td>'.$i.'</td>';
-      }
-         if ($i==$j) {
-            echo '<td>';
-         } else {
-            echo '<td>';
-         }
-      echo $i*$j;
-      echo '</td>';
-   }
-   echo '</tr>';
-   $j=1;
-}
-echo '</table>';
-*/
 /*$années=array(
    "Janvier"=> "31",
    "Février"=> "28",
