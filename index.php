538e3e30e05d1684c8fa47af8be037475baca432
Samir <samirb80440@gmail.com>
2024-06-05 15:54:31 +0200

    nvmodif

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP test</title>
+    <title>PHP exo</title>
 </head>
 <body>
 <?php
